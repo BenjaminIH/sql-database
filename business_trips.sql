424a96219d1e4237fd25400a318764f28e34b0d4
jipijipi <jp.leflot@gmail.com>
2024-10-04 00:18:12 +0200

    Sql tweaks

--- business_trips.sql
+++ business_trips.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS green_countries;
 CREATE TABLE green_countries AS
 SELECT 
     population.country, 
-    population.code, 
+    population.code,
+    population.population,
     aggregated.total_score
 FROM 
     population
@@ -13,15 +14,26 @@ LEFT JOIN (
         code, 
         SUM(score) AS total_score
     FROM (
-        SELECT code, score FROM travel
+        SELECT code, score * 5 AS score FROM travel
         UNION ALL
-        SELECT code, score FROM deforestation
+        SELECT code, score * 3 AS score  FROM deforestation
         UNION ALL
-        SELECT code, score FROM air_pollution
+        SELECT code, score * 10 AS score FROM air_pollution
         UNION ALL
-        SELECT code, score FROM energy
+        SELECT code, score * 6 AS score FROM energy
         UNION ALL
-        SELECT code, score FROM plastic_pollution
+        SELECT code, score * 4 AS score FROM plastic_pollution
     ) AS merged_topics
     GROUP BY code
 ) AS aggregated ON population.code = aggregated.code;
+
+DROP VIEW IF EXISTS green_countries_big;
+CREATE VIEW green_countries_big AS
+SELECT 
+    green_countries.country,
+    green_countries.code,
+    green_countries.population,
+    green_countries.total_score
+FROM 
+    green_countries 
+WHERE green_countries.population > 4000000;
