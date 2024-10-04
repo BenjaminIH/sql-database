86f233955acacc888410d6dbff699291865bd487
Pyros009 <Ricardo.f.morgado@gmail.com>
2024-10-04 13:57:49 +0100

    cleanup energy and functions

--- cleaning.py
+++ cleaning.py
@@ -13,10 +13,11 @@ import seaborn as sns
 def world_plot(df, llabel):
     # Load world shape data from geopandas
     world = gpd.read_file('maps/110m_cultural/ne_110m_admin_0_countries.shp')
-    df["code"] = df["code"].replace({"FRA":"FR1" , "NZL":"NZ1" , "FIN":"FI1" , "DNK":"DN1" , "CHN":"CH1" , "GBR":"GB1" , "USA":"US1" , "AUS":"AU1" , "NDL":"NL1" , "CUB":"CU1" , "KAZ":"KA1" , "SSD":"SDS" , "ESH":"SAH" , "ISR":"IS1"})
+    df2 = df.copy()
+    df2["code"] = df2["code"].replace({"FRA":"FR1" , "NZL":"NZ1" , "FIN":"FI1" , "DNK":"DN1" , "CHN":"CH1" , "GBR":"GB1" , "USA":"US1" , "AUS":"AU1" , "NDL":"NL1" , "CUB":"CU1" , "KAZ":"KA1" , "SSD":"SDS" , "ESH":"SAH" , "ISR":"IS1"})
     
     # Merge with the world GeoDataFrame
-    m1 = world.merge(df, how='left', left_on='SOV_A3', right_on='code')
+    m1 = world.merge(df2, how='left', left_on='SOV_A3', right_on='code')
 
     # Define a custom colormap without white
     colors = ['#1dbf1d', '#e30f0b'] 
@@ -37,3 +38,34 @@ def world_plot(df, llabel):
     ax.axis('off')
     plt.title(f'World Map by {llabel}')
     plt.show()
+    
+# Function to get the 3-letter country code
+def get_country_code(country_name):
+    try:
+        return pycountry.countries.lookup(country_name).alpha_3
+    except LookupError:
+        return None
+    
+# display energy graphics
+def graph_melt(df, title):
+    df_melted = df.melt(id_vars='country', 
+                    value_vars=['avg_biofuel', 'avg_coal', 'avg_hydro', 'avg_gas', 
+                                'avg_nuclear', 'avg_oil', 'avg_solar', 'avg_wind'],
+                    var_name='Energy Source', 
+                    value_name='Average Value')
+
+    threshold = 2  # Set your threshold value
+    df_filtered = df_melted[df_melted['Average Value'] >= threshold]
+
+    sns.barplot(data=df_filtered, x='country', y='Average Value', hue='Energy Source')
+
+    plt.title(title)
+    plt.xlabel('Country')
+    plt.ylabel('Average Value (%)')
+    plt.xticks(rotation=45)
+    plt.legend(title=title, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
+    
